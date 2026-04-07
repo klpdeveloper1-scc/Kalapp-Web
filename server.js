c41d8969f7f39dee3c66934df22f277f1697d709
klpdeveloper1 <klpdeveloper1@gmail.com>
2026-04-07 07:56:04 +0000

    mongo db setupchange

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ const storage = new CloudinaryStorage({
 const upload = multer({ storage: storage });
 
 // --- MongoDB Setup ---
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('✅ MongoDB Connected'))
     .catch(err => console.error('❌ MongoDB Error:', err));
 
